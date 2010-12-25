a5bc5a25ccf2cc28915b3466966e77c3f3d2dd7c
B V Satyaram <bvsatyaram@gmail.com>
2010-12-26 00:25:39 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbslap::Application.config.secret_token = 'ead7746dc31295b1fea596d755532aeb0239fe1ba9d853ecebd1f03dad0632423ca039b55024aad0733e867b6f4fd40845908a1adabaf3128cbb04dafbf0d5cd'
